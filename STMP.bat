bb20bf165c22e1fc3370e35d5e5d33834c6fecd3
RossAscends <124905043+RossAscends@users.noreply.github.com>
2023-12-15 13:54:36 +0900

    improve stmp.bat for secrets.json creation

--- STMP.bat
+++ STMP.bat
@@ -1,13 +1,27 @@
 @echo off
 pushd %~dp0
 
+ECHO ===========================
+ECHO SillyTavern MultiPlayer
+ECHO ===========================
+
+ECHO -- Looking for secrets.json....
+
 IF EXIST secrets.json ECHO Found secrets.json!
+ECHO ===========================
+IF EXIST secrets.json GOTO StartServer
 CALL :CreateNewIfMissing secrets.json
 
 :CreateNewIfMissing
 echo {"api_key_tabby":"_YOUR_API_KEY_HERE_"} > secrets.json
+echo secrets.json created -- PUT YOUR TABBY API KEY IN HERE AND RESTART THE SERVER
+ECHO ===========================
 
+:StartServer
+ECHO -- Checking/Installing Node Modules
 call npm install --no-audit
+ECHO ===========================
+ECHO -- Starting STMP server
 node server.js
 pause
 popd
